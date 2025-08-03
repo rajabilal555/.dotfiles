faf16114b27a44e572a9654e9b1838a605b7108b
Bilal Pervez <rajabilal555@gmail.com>
2025-08-03 15:12:33 +0500

    Terminal emulators configs added (Current: alacritty)

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Cai (Gogh)"
+config.font = wezterm.font("JetBrainsMono NFM")
+config.window_background_opacity = 0.8
+-- config.text_background_opacity = 0.6
+
+return config
